0fbd7c29d6ddfcdffb0c0cae8ddb4f40f31fba5c
Junt184 <1840107239@qq.com>
2025-12-11 20:12:33 +0800

    修改host0。0。0。0

--- main.py
+++ main.py
@@ -219,4 +219,4 @@ def convert_text(input_data: TextInput):
 if __name__ == "__main__":
     import uvicorn
     # 启动服务
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
